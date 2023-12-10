bfaad546bac501757031f9a831b0b8fb9e4f34a6
Sudikulal <sudikulal325@gmail.com>
2023-12-10 19:15:15 +0530

    Update:added timeline module

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,47 +1,78 @@
 import React from "react";
-import { experiences } from "../data/constants";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import { experiences, education } from "../data/constants";
+import { MdWork } from "react-icons/md";
+import { IoIosSchool } from "react-icons/io";
 
 function Experience() {
   return (
-    <>
-    <div id="experience" className="text-bg-dark">
-      <div className="container p-4">
-        <h1 className="pt-4 text-center">Experience</h1>
-        <p className="mt-2 text-center">My Work Experience as a Software Engineer</p>
-        <div className="row justify-content-center">
-          {experiences.map((experience, index) => (
-            <div className="col-lg-4 col-md-6 col-sm-12 mb-4" key={index}>
-              <div className="card h-100 text-bg-secondary">
-                <img
-                  src={experience.img}
-                  className="card-img-top bg-white"
-                  alt={experience.company}
-                  style={{ maxHeight: "150px", objectFit: "cover" }}
-                />
-                <div className="card-body d-flex flex-column justify-content-between">
-                  <div>
-                    <h5 className="card-title text-center">{experience.company}</h5>
-                    <h6 className="card-subtitle mb-2 text-center">
-                      {experience.role}
-                    </h6>
-                  </div>
-                  <div>
-                    <p className="card-text">
-                      <strong>Date:</strong> {experience.date}
-                      <br />
-                      <strong>Description:</strong> {experience.desc}
-                      <br />
-                      <strong>Skills:</strong> {experience.skills.join(", ")}
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
+    <div id="experience" className="text-bg-dark pt-5">
+      <h1 className="pt-4 text-center">Experience</h1>
+      <p className="mt-2 text-center">
+        My Work Experience as a Software Engineer
+      </p>
+      <div
+        className="container p-4"
+        data-aos="fade-up"
+        style={{ overflowY: "auto", height: "500px" }}
+      >
+        <VerticalTimeline className="custom-line">
+          {education.map((data, i) => (
+            <VerticalTimelineElement
+              key={i}
+              className="vertical-timeline-element--education"
+              contentStyle={{ background: "#343a40", color: "#fff" }}
+              contentArrowStyle={{
+                borderRight: "7px solid  rgb(33, 150, 243)",
+              }}
+              date={data.date}
+              iconStyle={{
+                background: "#0563bb",
+                color: "#fff",
+              }}
+              icon={<IoIosSchool />}
+            >
+              <h3 className="vertical-timeline-element-title h5 fw-bold">
+                {data.college}
+              </h3>
+              <h4 className="vertical-timeline-element-subtitle h6 fw-normal">
+                {data.degree}
+              </h4>
+              <p className="fs-6">{data.desc}</p>
+            </VerticalTimelineElement>
           ))}
-        </div>
+          {experiences.map((data, i) => (
+            <VerticalTimelineElement
+              key={i}
+              className="vertical-timeline-element--work"
+              contentStyle={{ background: "#343a40", color: "#fff" }}
+              contentArrowStyle={{
+                borderRight: "7px solid  rgb(33, 150, 243)",
+              }}
+              date={data.date}
+              dateClassName="timeline-date"
+              iconStyle={{
+                background: "#0563bb",
+                color: "#fff",
+              }}
+              icon={<MdWork />}
+            >
+              <h3 className="vertical-timeline-element-title h5 fw-bold">
+                {data.company}
+              </h3>
+              <h4 className="vertical-timeline-element-subtitle h6 fw-normal">
+                {data.role}
+              </h4>
+              <p className="fs-6">{data.desc}</p>
+            </VerticalTimelineElement>
+          ))}
+        </VerticalTimeline>
       </div>
     </div>
-    </>
   );
 }
 
