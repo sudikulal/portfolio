dc7e5c80559423f69d0d97e5b7cfd75f690a50b0
Sudikulal <sudikulal325@gmail.com>
2023-12-24 22:14:23 +0530

    Update:added parralax effect

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,20 +1,77 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Lottie from "lottie-react";
 import animationData from "../assets/Animation.json";
 import { Bio } from "../data/constants";
 import Typewriter from "typewriter-effect";
 import banner from "../assets/bg.png";
+import lax from "lax.js";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 function About() {
+  useEffect(() => {
+    // Setup AOS
+    AOS.init();
+
+    // Setup Lax
+    lax.init();
+
+    lax.addDriver("scrollY", function () {
+      return window.scrollY;
+    });
+
+  // Add lax effect for the first column
+  lax.addElements(".info", {
+    scrollY: {
+      translateX: [
+        [0, "elOutX"],
+        [0, -3000],
+        {
+          inertia: 1,
+        },
+      ],
+    },
+  });
+
+  // Add lax effect for the second column
+  lax.addElements(".lottie", {
+    scrollY: {
+      translateX: [
+        [0, "elOutX"],
+        [0, 3000],
+        {
+          inertia: 1,
+        },
+      ],
+      rotate: [
+        [0, 1e9],
+        [0, 1e9]
+      ]
+    },
+  });
+  }, []);
+
+  useEffect(() => {
+    AOS.refresh();
+  }, []);
+
   return (
     <div
       id="about"
       className="bg-dark text-light p-5 mt-4"
-      style={{ backgroundImage: `url(${banner})`, backgroundSize: "cover", backgroundBlendMode:"soft-light"}}
+      style={{
+        backgroundImage: `url(${banner})`,
+        backgroundSize: "cover",
+        backgroundBlendMode: "soft-light",
+      }}
     >
       <div className="container mt-4 p-4">
         <div className="row  py-4">
-          <div className="col-lg-6">
+          <div
+            className="info col-lg-6"
+            data-aos="fade-right"
+            data-aos-duration="1000"  // Adjust duration as needed
+          >
             <header>
               <h1 className="mb-4">
                 Hi, I am <br /> <strong>{Bio.name}</strong>
@@ -45,7 +102,11 @@ function About() {
               </button>
             </div>
           </div>
-          <div className="col-lg-6 d-flex justify-content-end align-items-center">
+          <div
+            className="lottie col-lg-6 d-flex justify-content-end align-items-center"
+            data-aos="fade-left"
+            data-aos-duration="1000"  // Adjust duration as needed
+          >
             <Lottie
               animationData={animationData}
               loop={true}
